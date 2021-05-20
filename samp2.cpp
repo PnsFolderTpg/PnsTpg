2c3b5151361cc85968e3282f96e41992f3192825
PnsFolderTpg <67183281+PnsFolderTpg@users.noreply.github.com>
2021-05-20 19:18:46 +0530

    Add files via upload

---
+++ samp2.cpp
@@ -0,0 +1,57 @@
+// Balanced paranthasys 
+
+#include<iostream>
+//#include<bits/stdc++>
+#include<stack>
+#include<vector>
+#include<string>
+using namespace std;
+
+int main(){
+	//int t;
+	char i;
+	//cin>>t;
+	//while(t--)
+	//{
+		string s;
+		cin>>s;
+		int fl=1;
+		stack<char>st;
+		//cout<<s.length()<<endl;
+		for(int ch=0;ch<s.length();ch++) //for(auto i:s)
+		{
+			i=s[ch];
+			//cout<<i<<endl;
+			if(i=='[' || i=='{' || i=='(')
+			{			
+				st.push(i);
+			}
+			else{
+				if(i==']' && !st.empty() && st.top()=='[')
+				{
+					st.pop();
+				}
+				else if(i=='}' && !st.empty() && st.top()=='{')
+				{
+					st.pop();
+				}
+				else if (i==')' && !st.empty() && st.top()=='(')
+				{
+					st.pop();
+				}
+				else{
+					fl=0;
+				}
+			}
+		}
+		if (st.size()!=0 || fl==0)
+		{
+			cout<<"false";
+		}
+		else
+		{
+			cout<<"true";
+		}
+	//}
+	return 0;
+}
